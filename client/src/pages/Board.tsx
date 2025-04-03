592d0eace06f60812dd240962c59ec2a8527da7a
KnifeDad <boopyshnoot@gmail.com>
2025-04-03 00:28:09 -0400

    Fix: Remove unused Link import from Board component

--- client/src/pages/Board.tsx
+++ client/src/pages/Board.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
 import { retrieveTickets, deleteTicket } from '../api/ticketAPI';
 import { retrieveUsers } from '../api/userAPI';
 import ErrorPage from './ErrorPage';
